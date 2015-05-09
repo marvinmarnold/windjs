f66ff8b6204a618bbdd736dd164b257c951c5da7
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-09 13:39:28 -0500

    Moved bluetooth code into cordova block

--- windjs.js
+++ windjs.js
@@ -1,12 +1,18 @@
 if (Meteor.isClient) {
   Template.body.events({
-      "submit .new-message": function (e) {
-        var text = e.target.text.value;
-        Meteor.call("setName", text);
-        e.target.text.value = "";
+    "submit .new-message": function (e) {
+      var text = e.target.text.value;
+      Meteor.call("setName", text);
+      e.target.text.value = "";
 
-        return false;
-      }
+      return false;
+    },
+    // "click .get-messages": function (e) {
+      // var devices = Meteor.call("getMessages");
+      // devices.forEach(function(device) {
+      //   console.log(device.id);
+      // });
+    // }
   });
 }
 
@@ -14,10 +20,14 @@ if (Meteor.isCordova) {
   Meteor.startup(function () {
     bluetoothSerial.enable();
   });
+  Meteor.methods({
+    setName: function (text) {
+      bluetoothSerial.setName(text);
+    },
+    // getMessages: function() {
+    //   return bluetoothSerial.discoverUnpaired(function(devices) {
+    //     return devices;
+    //   },null);
+    // }
+  });
 }
-
-Meteor.methods({
-  setName: function (text) {
-    bluetoothSerial.setName(text);
-  }
-});
