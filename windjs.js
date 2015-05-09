6a6e86ae82dd09e0c576da366667a298ad42664c
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-09 15:49:31 -0500

    No dups

--- windjs.js
+++ windjs.js
@@ -1,10 +1,17 @@
 Chimes = new Mongo.Collection("chimes");
 
 if (Meteor.isClient) {
+  Meteor.startup(function() {
+    setInterval(function() {
+      Meteor.call("bridgeGetMessages");
+    }, 5000);
+  });
+
   Template.body.events({
     "submit .new-message": function (e) {
+      var username = e.target.username.value;
       var text = e.target.text.value;
-      Meteor.call("bridgeSetName", text);
+      Meteor.call("bridgeSetName", username, text);
       e.target.text.value = "";
 
       return false;
@@ -26,9 +33,11 @@ if (Meteor.isCordova) {
     bluetoothSerial.enable();
   });
   Meteor.methods({
-    bridgeSetName: function (text) {
+    bridgeSetName: function (username, text) {
+      var createdAt = new Date().getTime();
       console.log("Changing the name");
-      bluetoothSerial.setName("@" + (new Date().getTime())+"##"+text);
+      bluetoothSerial.setName("@" + username + ":" + createdAt + "##" + text);
+      Meteor.call("createChime", "self", username, createdAt, createdAt, text);
     },
     bridgeGetMessages: function() {
       console.log("Getting messages");
@@ -38,30 +47,59 @@ if (Meteor.isCordova) {
           console.log("New device");
           Meteor.call("addChime", device.id, device.name);
         });
-      },null);
+      }, null);
     }
   });
 }
 
 Meteor.methods ({
+  createChime: function(userID, username, discoveredAt, createdAt, text){
+    Chimes.insert({
+      text: text,
+      createdAt: createdAt,
+      discoveredAt: discoveredAt,
+      username: username,
+      userID: userID
+    })
+  },
   addChime: function(userID, bluetoothDeviceName) {
     if(bluetoothDeviceName && bluetoothDeviceName.search(/^@/) != -1) {
-      console.log("Adding chime");
-      Chimes.insert({
-        text: parseText(bluetoothDeviceName),
-        discoveredAt: new Date(),
-        createdAt: parseCreatedAt(bluetoothDeviceName),
-        userID: userID
+      var text = parseText(bluetoothDeviceName);
+      var createdAt = parseCreatedAt(bluetoothDeviceName);
+      var username = parseUsername(bluetoothDeviceName);
+
+      var dups = Chimes.find({
+        $and: [
+          {text: text},
+          {userID: userID},
+          {createdAt: createdAt}
+        ]
       });
+
+      if(dups.count() === 0) {
+        console.log("Adding chime");
+        Chimes.insert({
+          text: text,
+          discoveredAt: new Date(),
+          createdAt: createdAt,
+          username: username,
+          userID: userID
+        });
+      }
     }
   },
 
 });
 
+// @krainboltgreene:12345##message
 var parseText = function(name) {
   return name.split("##")[1];
-}
+};
 
 var parseCreatedAt = function(name) {
-  return name.split("##")[0];
+  return name.split("##")[0].split(":")[1];
+};
+
+var parseUsername = function(name) {
+  return name.split(":")[0]
 }
