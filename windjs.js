058e0b9e0c77d2a5cee6c210c5cfbfeac7df7b67
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-09 14:46:37 -0500

    Working

--- windjs.js
+++ windjs.js
@@ -2,32 +2,51 @@ if (Meteor.isClient) {
   Template.body.events({
     "submit .new-message": function (e) {
       var text = e.target.text.value;
-      Meteor.call("setName", text);
+      Meteor.call("bridgeSetName", text);
       e.target.text.value = "";
 
       return false;
     },
-    // "click .get-messages": function (e) {
-      // var devices = Meteor.call("getMessages");
-      // devices.forEach(function(device) {
-      //   console.log(device.id);
-      // });
-    // }
+    "click .get-messages": function (e) {
+      Meteor.call("bridgeGetMessages");
+    }
+  });
+
+  Meteor.methods({
+    createMessage: function(device) {
+      $('ul').append("<li>" + device.id + "</li>");
+    }
   });
 }
+
+// if(Meteor.isServer) {
+//   Meteor.methods({
+//     setName: function(text) {
+//       Meteor.call("bridgeSetName", text);
+//     log: function(text) {
+//       console.log(text);
+//     }
+//   });
+// }
 
 if (Meteor.isCordova) {
   Meteor.startup(function () {
     bluetoothSerial.enable();
   });
   Meteor.methods({
-    setName: function (text) {
+    bridgeSetName: function (text) {
+      console.log("Changing the name");
       bluetoothSerial.setName(text);
     },
-    // getMessages: function() {
-    //   return bluetoothSerial.discoverUnpaired(function(devices) {
-    //     return devices;
-    //   },null);
-    // }
+    bridgeGetMessages: function() {
+      console.log("Getting messages");
+      bluetoothSerial.discoverUnpaired(function(devices) {
+        console.log("Returnning devices");
+        devices.forEach(function(device) {
+          console.log("New device");
+          Meteor.call("createMessage", device);
+        });
+      },null);
+    }
   });
 }
